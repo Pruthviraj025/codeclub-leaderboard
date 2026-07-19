aa2b4ce09bdeba71bb7aba0637794382a9ef7028
Pruthviraj025 <ptvrj25@gmail.com>
2026-07-19 11:50:12 +0530

    Remove HistoryPage route from App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LeaderboardPage from './pages/LeaderboardPage';
-import HistoryPage from './pages/HistoryPage';
 import ProfilePage from './pages/ProfilePage';
 import AdminPage from './pages/AdminPage';
 
@@ -11,7 +10,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/leaderboard" element={<LeaderboardPage />} />
-        <Route path="/history" element={<HistoryPage />} />
         <Route path="/profile/:userId" element={<ProfilePage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
