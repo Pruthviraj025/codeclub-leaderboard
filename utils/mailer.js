4cb5050963702419536a0a7677eb57f3b5827a2c
Pruthviraj <ptvrj25@gmail.com>
2026-08-01 21:11:16 +0530

    fix ENETUNREACH: force ipv4 for smtp

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,9 +1,13 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+
+dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT) || 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
